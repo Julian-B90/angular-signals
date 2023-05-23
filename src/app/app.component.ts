b5c8e30f3c88906cbad9a867952652e6fe4e0c08
Julian Schmidt <info@julianb.de>
2023-05-23 15:29:07 +0200

    test signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+const originalTodos = [
+  { id: 1, title: 'Learn signals', done: false },
+  { id: 2, title: 'Foo', done: false },
+  { id: 3, title: 'Bar', done: false },
+  { id: 4, title: 'Klasse', done: false },
+  { id: 5, title: 'Klaus', done: false },
+];
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  myModel: any;
+
+  todos = signal(originalTodos);
+
+  onlyDoneTodo = computed(() => {
+    return this.todos().filter((item) => !item.done);
+  });
+  doneTodo = computed(() => {
+    return this.todos().filter((item) => item.done);
+  });
+
   title = 'my-app-signals';
+
+  public updateTitle() {
+    this.todos.mutate((value) => {
+      value[3].title = 'Das ist ein Test';
+    });
+  }
+
+  public addTodo() {
+    const nextId = this.todos().length + 1;
+    this.todos.update((value) => {
+      value.push({ id: nextId, done: false, title: this.myModel });
+      return value;
+    });
+  }
+
+  public delteTodo(id: number) {
+    this.todos.update((value) => {
+      return value.filter((item) => item.id !== id);
+    });
+  }
+
+  public done(id: number) {
+    this.todos.mutate((value) => {
+      const findInex = value.findIndex((item) => item.id === id);
+      if (findInex !== -1) {
+        value[findInex].done = true;
+      }
+    });
+  }
+
+  public undone(id: number) {
+    this.todos.mutate((value) => {
+      const findInex = value.findIndex((item) => item.id === id);
+      if (findInex !== -1) {
+        value[findInex].done = false;
+      }
+    });
+  }
+
+
+  public replaceAll() {
+    this.todos.set([
+      { id: 1, title: 'bla', done: false },
+    ]);
+  }
 }
